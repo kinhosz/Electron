72d8251ebdb052d034003dfb75ea427af2111cb9
kinhosz <jcsc@cin.ufpe.br>
2020-09-27 23:50:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
